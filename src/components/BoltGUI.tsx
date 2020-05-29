2e124a3bbcb009ad8a593e1503d94ebab081d821
Sean Mcc <26191159+mcc12@users.noreply.github.com>
2020-05-29 15:06:58 +0100

    add basic user interface (#2)
    * add basic user interface
    
    * refactor button input
    
    * slight improvement to folder structure (#3)
    
    * refactor row layout component
    
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>

---
+++ src/components/BoltGUI.tsx
@@ -0,0 +1,274 @@
+import { ButtonInputComponent, Label, TextInputComponent } from './inputs';
+import React, { Component } from 'react';
+
+import { MainPanel } from './panels/MainPanel';
+import { TabData } from './panels/ContentPanel';
+import goodboyLogo from '../assets/goodboy-logo.png';
+import { render } from 'react-dom';
+
+export interface BoltProps
+{
+    stage?: any; // this will be the reference object for the game if needed
+}
+
+export interface BoltState
+{
+    expandedX: boolean;
+    expandedY: boolean;
+    activeTab: number;
+}
+
+export class BoltGUI extends Component<BoltProps, BoltState>
+{
+    private tabs: TabData[] = [];
+
+    constructor(props: BoltProps)
+    {
+        super(props);
+        this.state = {
+            expandedX: true,
+            expandedY: true,
+            activeTab: 0,
+        };
+
+        const logInputChange = (value:any) => console.log(value);
+
+        this.tabs.push({
+            title: 'Example',
+            id: 'E',
+            // idColour: 'red',
+            elements: [
+                {
+                    rows: [
+                        {
+                            components: [
+                                { component: Label, inputData: { label: 'Hello world! Welcome to Bolt.gui' }},
+                            ]
+                        },
+                    ]
+                },
+                {
+                    label: 'Button example',
+                    rows: [
+                        {
+                            components: [
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    imgSrc: goodboyLogo,
+                                    imgAlt: 'test',
+                                    inputBackgroundColour: 'red',
+                                    disabled: true,
+                                    buttonText: 'Clicky',
+                                    callOnClick: () => { console.log('ive been clicked')}
+                                }}
+                            ]
+                        },
+                    ]
+                },
+                {
+                    label: 'Two buttons',
+                    rows: [
+                        {
+                            components: [
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    buttonText: 'Option 1',
+                                    callOnClick: () => { console.log('ive been clicked')},
+                                    inputWidth: '98%'
+                                }}
+                            ]
+                        },
+                        {
+                            components: [
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    buttonText: 'Option 2',
+                                    callOnClick: () => { console.log('ive been clicked')},
+                                    inputWidth: '98%'
+                                }}
+                            ]
+                        },
+                    ]
+                },
+
+            ],
+        })
+        this.tabs.push({
+            title: 'Level Data',
+            // tabImg: goodboyLogo,
+            // imgAlt: 'hellooooo',
+            elements: [
+                {
+                    label: '2D Position',
+                    rows: [
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'x', inputWidth: '95%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'y', inputWidth: '95%' }}
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: '3D Position',
+                    rows: [
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'x', inputWidth: '92%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'y', inputWidth: '92%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'z', inputWidth: '92%' }}
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: 'Cursor colour',
+                    rows: [
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'r', inputWidth: '95%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'g', inputWidth: '95%' }}
+                            ]
+                        },
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'b', inputWidth: '95%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, label: 'a', inputWidth: '95%' }}
+                            ]
+                        },
+                    ]
+                }
+            ],
+        })
+        this.tabs.push({
+            title: 'SoundBoy Data',
+            id: 'S',
+            elements: [
+                {
+                    label: 'Multi-row',
+                    rows: [
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '98%' }}
+                            ]
+                        },
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '92%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '92%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '92%' }}
+                            ]
+                        },
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '98%' }}
+                            ]
+                        },
+                        {
+                            components: [
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '95%' }},
+                                { component: TextInputComponent, inputData: { callOnChange: logInputChange, inputWidth: '95%' }}
+                            ]
+                        },
+                    ]
+                },
+                {
+                    label: 'Use Bolt',
+                    rows: [
+                        {
+                            components: [
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    inputBackgroundColour: 'green',
+                                    buttonText: 'yes',
+                                    callOnClick: () => { console.log('ive been clicked')},
+                                    inputWidth: '97%',
+                                    inputHeight: '45px'
+                                }},
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    inputBackgroundColour: 'red',
+                                    buttonText: 'No',
+                                    callOnClick: () => { console.log('ive been clicked')},
+                                    inputWidth: '97%',
+                                    inputHeight: '45px'
+                                }}                                
+                            ]
+                        },
+                        {
+                            components: [
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    buttonText: 'Maybe?',
+                                    callOnClick: () => { console.log('ive been clicked')},
+                                    inputWidth: '97%',
+                                    inputHeight: '45px'
+                                }},
+                                {
+                                    component: ButtonInputComponent, inputData: {
+                                    buttonText: 'Repeat question',
+                                    callOnClick: () => { console.log('ive been clicked')},
+                                    inputWidth: '97%',
+                                    inputHeight: '45px'
+                                }}                               
+                            ]
+                        },
+                    ]
+                },
+            ],
+        })
+    }
+
+    // public componentWillMount()
+    // {
+    // }
+
+    // public componentWillUnmount()
+    // {
+
+    // }
+
+    public render(): JSX.Element
+    {
+        return (
+            <MainPanel
+                expandedX={this.state.expandedX}
+                expandedY={this.state.expandedY}
+                expandYCallBack={this.toggleExpandedY.bind(this)}
+                setActiveTabCallBack={this.setActiveTab.bind(this)}
+                tabData={this.tabs}
+                activeTabIndex={this.state.activeTab}
+            />
+        );
+    }
+
+    public toggleExpandedX(): void
+    {
+        this.setState({ expandedX: !this.state.expandedX });
+    }
+
+    public toggleExpandedY(): void
+    {
+        this.setState({ expandedY: !this.state.expandedY });
+    }
+
+    public setActiveTab(newState: number): void
+    {
+        this.setState({ activeTab: newState});
+    }
+
+    // STATIC MEMBERS --------------------------------------------------
+
+    public static init(): void // (editor: Editor) - this is where we would pass in the game
+    {
+        let element = document.getElementById('editor-holder');
+
+        if (!element)
+        {
+            element = document.createElement('div');
+            element.id = 'editor-holder';
+            document.body.appendChild(element);
+        }
+        render(<BoltGUI/>, element);
+    }
+}
---
+++ src/components/panels/ContentPanel.tsx
@@ -0,0 +1,110 @@
+import { ColumnLayout, ComponentPair, RowLayout } from '../inputs/layouts';
+import React, { FunctionComponent } from 'react';
+
+import styled from 'styled-components';
+
+export interface TabData {
+    /** title of tab */
+    title: string;
+    /** tab id, abbreviation to be displayed in side bar */
+    id?: string;
+    /** optional over-ride for tab button text colour */
+    idColour?: string;
+    /** if provided, replaces tab button text with image */
+    tabImg?: string;
+    /** alt text for replaced image */
+    imgAlt?: string;
+    /** optional background colour for tab button */
+    tabColour?: string;
+    /** components to be displayed under tab */
+    elements: ElementData[];
+}
+
+export interface ElementData {
+    label?: string,
+    labelColour?: string,
+    labelFont?: string,
+    rows: {
+        components: ComponentPair[]
+    }[];
+}
+
+export interface ContentPanelProps
+{
+    /** panel width, defaults to '400px' */
+    panelWidth?: string;
+    /** Information to be rendered in content panel */
+    tabData: TabData;
+}
+
+interface ContainerProps {
+    panelWidth?: string;
+}
+
+const Container = styled.div<ContainerProps>`
+    width: ${(props: ContainerProps): string => (props.panelWidth ? props.panelWidth : '400px')};
+    transition: width 0.25s, height 0.25s;
+    background-color: #252C39;
+    border-radius: 0px 5px 5px 0px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {display:none;}
+`
+
+const Header = styled.div`
+    width: 100%;
+    margin: 0;
+    padding: 5px 0 5px 10px;
+    border-radius: 0px 5px 0 0;
+    background-color: #5C65BC;
+    display: flex;
+`
+
+const TitleText = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: white;
+    opacity: 0.8;
+    font-family: Roboto;
+    font-size: 36px;
+`
+
+const ElementContainer = styled.div`
+    width: 100%;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {display:none;}
+`
+
+const ComponentWrapper = styled.div`
+    width: 100%;
+`
+
+export const ContentPanel: FunctionComponent<ContentPanelProps> = (props: ContentPanelProps) =>
+{
+    const data = props.tabData;
+    return (
+        <Container
+            panelWidth={props.panelWidth}
+        >
+            <Header>
+                <TitleText>{data.title.toUpperCase()}</TitleText>
+            </Header>
+            <ElementContainer>
+                {
+                    data.elements.map((element) => {
+                    return (
+                        <ComponentWrapper>
+                            <ColumnLayout
+                                label={element.label}
+                                labelColour={element.labelColour}
+                                labelFontSize={element.labelColour}
+                            >
+                                <RowLayout rows={element.rows}/>
+                            </ColumnLayout>
+                        </ComponentWrapper>
+                    )
+                    })
+                }
+            </ElementContainer>
+        </Container>
+    );
+}
